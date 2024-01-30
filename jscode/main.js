c3801c601e034020b53d0e54e0ab319eb6d8d491
Ahmed El-Kady <ahmed_el_kady2000@yahoo.com>
2024-01-30 12:12:37 +0200

    30-1-2024--1150am

--- jscode/main.js
+++ jscode/main.js
@@ -1,5 +1,6 @@
 const my1btn = document.getElementById("imgbtn");
 const sectionImgviewer = document.querySelector("section.imgviewer");
+const topBtn = document.getElementById("goZero")
 
 const singelimg = document.getElementById("singelimg");
 
@@ -24,17 +25,16 @@ let imgArry2 = [
   "../img/aswan1-img.webp",
   "../img/aswan2-img.webp",
 ];
-
+topBtn.addEventListener("click", ()=>{  window.scrollTo({ top: 0, behavior: 'smooth' })
+})
 my1btn.onclick = () => {
   sectionImgviewer.classList.remove("imgviewerok");
   singelimg.innerHTML = ``;
 };
-
 function showImg(idx, alt) {
   singelimg.innerHTML = `<img src="${imgArry[idx]}" alt="${alt}">`;
   sectionImgviewer.classList.add("imgviewerok");
 }
-
 function showImg2(idx, alt) {
   singelimg.innerHTML = `<img src="${imgArry2[idx]}" alt="${alt}">`;
   sectionImgviewer.classList.add("imgviewerok");
